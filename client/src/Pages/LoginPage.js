ce4200e4d63d46a91f93f46a8efbe8d7b45e3c85
Donggun Lee <donggunlee@Dongguns-MacBook-Pro.local>
2024-04-27 16:58:36 +0900

    half screen

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,36 +1,36 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
-import '/Users/donggunlee/Desktop/CloneBuilder/URP/client/src/Component/Login/login.css';
-
+import '/Users/donggunlee/Desktop/CloneBuilder/URP/client/src/Component/login.css';
 
 
 export const LoginPage = (props) => {
     return(
         <>
-        <div className='signholder'>
-                <form className='signupbox'>
-                    <form className='signinTitle'>
-                        Create My Digital Clone
-                    </form>
-                    <input className='signInput' placeholder={'Username'} ></input>
-                    <input className='signInput' placeholder={'Password'} ></input>
-                    <div className='buttonContainer'>
-                        <button className='loginBtn' type="submit">Login</button>
-                        <button className='signBtn' type="submit">Sign In</button>
-                    </div>
-
-
-
-
-                </form>
-
-
-
-
-
-            </div>
-
         
+        <div className='outside'>   
+            <div className='signholder'>
+                    <form className='signupbox'>
+                        <form className='signinTitle'>
+                            Create My Digital Clone
+                        </form>
+                        <input className='signInput' placeholder={'Username'} ></input>
+                        <input className='signInput' placeholder={'Password'} ></input>
+                        <div className='buttonContainer'>
+                            <button className='loginBtn' type="submit">Login</button>
+                            <button className='signBtn' type="submit">Sign In</button>
+                        </div>
+
+
+
+                    </form>
+
+
+
+
+
+                </div>
+
+        </div>
         </>
     );
 }
--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
+import '/Users/donggunlee/Desktop/CloneBuilder/URP/client/src/Component/main.css' ;
 
 export const MainPage = (props) => {
     return (
-    <>
-        mainpage
+    <> 
+      <div className='outside'> 
+        <div className='left-side'></div>
+        <div className='right-side'></div>
+        
+
+      
+      </div>  
+   
+
+
+
+
     </>);
 }
