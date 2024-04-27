ac59a7bac4fd96bb990341c603e955f073cea805
Donggun Lee <donggunlee@Dongguns-MacBook-Pro.local>
2024-04-27 15:25:10 +0900

    Login Page

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -2,30 +2,35 @@ import React from 'react'
 // import {Link} from 'react-router-dom'
 import '/Users/donggunlee/Desktop/CloneBuilder/URP/client/src/Component/Login/login.css';
 
+
+
 export const LoginPage = (props) => {
     return(
         <>
-    
-       <div className='signholder'>
+        <div className='signholder'>
                 <form className='signupbox'>
-                    <div className='signinTitle'>
+                    <form className='signinTitle'>
                         Create My Digital Clone
-                    </div>
+                    </form>
                     <input className='signInput' placeholder={'Username'} ></input>
                     <input className='signInput' placeholder={'Password'} ></input>
-                    <button className='loginBtn' type="submit">Sign In</button>
+                    <div className='buttonContainer'>
+                        <button className='loginBtn' type="submit">Login</button>
+                        <button className='signBtn' type="submit">Sign In</button>
+                    </div>
 
 
-                 
-                    
-                    
+
+
                 </form>
-        
+
 
 
 
 
             </div>
+
+        
         </>
     );
 }
